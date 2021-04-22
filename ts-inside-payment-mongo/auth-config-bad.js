dc4f96090f146e1ec31e849b2e39fb1a121fa30b
Dominik Huber <elcpt@elcpt.de>
2021-04-22 11:36:14 +0200

    Pre-init broken database to improve semantics

--- ts-inside-payment-mongo/auth-config-bad.js
+++ ts-inside-payment-mongo/auth-config-bad.js
@@ -8,3 +8,21 @@ db.createUser({
         }
     ]
 });
+// Add initial rows as well, due to semantic TT errors
+db.payment.insert({
+    "_id": "AFA31B7A9E76444ABA82E8896401CBFA",
+    "_class": "inside_payment.entity.Payment",
+    "orderId": "a27841df-7419-4ce5-9a51-283fdc335170",
+    "userId": "d8da2293-a030-454a-9d10-8eeb5b7e4ace",
+    "price": "50.0",
+    "type": "O"
+});
+db.payment.insert({
+    "_id":
+        "BA9051CFE77644C580CEA488F2A82EC1", "_class":
+        "inside_payment.entity.Payment", "orderId":
+        "d7481c0e-d727-4fe8-8e76-11ee3b2ba117", "userId":
+        "d8da2293-a030-454a-9d10-8eeb5b7e4ace", "price":
+        "50.0", "type":
+        "O"
+});
